dcfebf6e6b148a14aed7a26a23e08633e2046bdc
pranpreya <pranpreya.sama@gmail.com>
2019-12-23 18:09:49 +0700

    Update

--- app/src/main/java/com/example/petping/SearchFragment.java
+++ app/src/main/java/com/example/petping/SearchFragment.java
@@ -4,15 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SearchFragment extends Fragment {
+
+    private Spinner spinBreed;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       return inflater.inflate(R.layout.fragment_search, container,false);
+        final View temp = inflater.inflate(R.layout.fragment_search, null);
+
+        spinBreed = temp.findViewById(R.id.breed_spin);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.breed_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        //Can choose design of drop down
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinBreed.setAdapter(adapter);
+
+        return temp;
     }
 }
