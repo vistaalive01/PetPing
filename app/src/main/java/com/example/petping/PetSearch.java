a667bf0122d586e4f9fdb45b383a89031a273e23
pranpreya <pranpreya.sama@gmail.com>
2019-12-27 01:47:38 +0700

    petTypeSeaerch

--- app/src/main/java/com/example/petping/PetSearch.java
+++
@@ -1,4 +0,0 @@
-package com.example.petping;
-
-public class PetSearch {
-}
--- app/src/main/java/com/example/petping/SearchFragment.java
+++ app/src/main/java/com/example/petping/SearchFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -40,7 +41,11 @@ public class SearchFragment extends Fragment {
     private Button searchButton;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+    private String breed;
     private List<String> petSearchType = new ArrayList<>();
+    private List<String> breedSearchType = new ArrayList<>();
+    private List<String> searchResult = new ArrayList<>();
+    private ArrayAdapter<CharSequence> breedAdapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -57,7 +62,7 @@ public class SearchFragment extends Fragment {
         searchButton = temp.findViewById(R.id.pet_search);
 
         spinBreed = temp.findViewById(R.id.breed_spin);
-        final ArrayAdapter<CharSequence> breedAdapter = ArrayAdapter.createFromResource(getContext(), R.array.breed_array, android.R.layout.simple_spinner_item);
+        breedAdapter = ArrayAdapter.createFromResource(getContext(), R.array.breed_array, android.R.layout.simple_spinner_item);
         breedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinBreed.setAdapter(breedAdapter);
 
@@ -73,45 +78,86 @@ public class SearchFragment extends Fragment {
                 String dog = dogBox.getText().toString();
                 String cat = catBox.getText().toString();
                 String rabbit = rabbitBox.getText().toString();
-                petTypeBox(dog, cat, rabbit);
+                petTypeChoose(dog, cat, rabbit);
 
-
-
-
-                //Log.d("Test3", dogBox.getText().toString());
+                String breed1 = breedAdapter.getItem(0).toString();
+                String breed2 = breedAdapter.getItem(1).toString();
+                String breed3 = breedAdapter.getItem(2).toString();
+                petBreedChoose();
+                searchPetResult();
+                searchResult.clear();
             }
         });
 
         return temp;
     }
 
-    private void petTypeBox(String dog, String cat, String rabbit) {
-        if(dogBox.isChecked()) petSearchType.add(dog);
-        else petSearchType.remove(dog);
 
-        if(catBox.isChecked()) petSearchType.add(cat);
-        else petSearchType.remove(cat);
 
-        if(rabbitBox.isChecked()) petSearchType.add(rabbit);
-        else petSearchType.remove(rabbit);
-        for(int i = 0; i < petSearchType.size(); i++){
+   private void petBreedChoose() {
+        spinBreed.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                breed = breedAdapter.getItem(position).toString();
+                breedSearchType.add(breed);
+                searchResult.add(breed);
+                Log.d("Breed", breed);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    private void petTypeChoose(String dog, String cat, String rabbit) {
+        if(dogBox.isChecked()){
+            petSearchType.add(dog);
+            searchResult.add(dog);
+        }
+        else{
+            petSearchType.remove(dog);
+        }
+
+        if(catBox.isChecked()){
+            petSearchType.add(cat);
+            searchResult.add(cat);
+        }
+        else{
+            petSearchType.remove(cat);
+        }
+
+        if(rabbitBox.isChecked()) {
+            petSearchType.add(rabbit);
+            searchResult.add(rabbit);
+        }
+        else {
+            petSearchType.remove(rabbit);
+        }
+
+    }
+
+    private void searchPetResult(){
+
+        for(int i = 0; i < searchResult.size(); i++){
             db.collection("Pet")
-                    .whereIn("Type", Arrays.asList(petSearchType.get(i)))
+                    .whereEqualTo("Breed","ไทย")
+                    .whereIn("Type", Arrays.asList(searchResult.get(i)))
                     .get()
                     .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
                             if (task.isSuccessful()) {
                                 for (QueryDocumentSnapshot document : task.getResult()) {
-                                    Log.d("Test1", document.getId() + " => " + document.getData());
+                                    Log.d("DataTest", document.getId() + " => " + document.getData());
                                 }
                             } else {
-                                Log.d("Test2", "Error getting documents: ", task.getException());
+                                Log.d("Error", "Error getting documents: ", task.getException());
                             }
                         }
                     });
         }
-
 
     }
 
