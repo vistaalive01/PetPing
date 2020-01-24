5d9273c0aa9603c582d716d6cf19b789bae2def1
pranpreya <pranpreya.sama@gmail.com>
2020-01-24 21:36:19 +0700

    update 24/1 21:36

--- app/src/main/java/com/example/petping/AdoptionInfoFragment.java
+++ app/src/main/java/com/example/petping/AdoptionInfoFragment.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AdoptionInfoFragment extends Fragment {
+    private EditText eNID, eDOB, eAddr, eJob, eSalary;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_adoption_info_process, null);
+        eNID = view.findViewById(R.id.edit_info_nid);
+        eDOB = view.findViewById(R.id.edit_info_dob);
+        eAddr = view.findViewById(R.id.edit_info_addr);
+        eSalary = view.findViewById(R.id.edit_info_salary);
         return view;
     }
 }
