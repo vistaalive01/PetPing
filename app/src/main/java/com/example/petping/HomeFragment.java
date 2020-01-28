73f18e0b411e68eb57e96a79469beff91823a7a9
pranpreya <pranpreya.sama@gmail.com>
2020-01-28 20:26:12 +0700

    update 28/1 20:25

--- app/src/main/java/com/example/petping/HomeFragment.java
+++ app/src/main/java/com/example/petping/HomeFragment.java
@@ -13,6 +13,7 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       return inflater.inflate(R.layout.fragment_home, container,false);
+        View view = inflater.inflate(R.layout.fragment_home, null);
+        return view;
     }
 }
